7b31b9c3c9bb57e597847a33ca03db2ccf57ffb9
RHsanto <rakibulsanto789@gmail.com>
2022-12-17 18:00:09 +0600

    add api

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
-const express = require('express');
-const cors = require('cors')
-const app =express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 8000;
-require('dotenv').config();
+require("dotenv").config();
 
 //midalware
 app.use(cors());
@@ -14,7 +13,11 @@ app.use(express.json());
 console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z7kch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
 //   // perform actions on the collection object
@@ -22,7 +25,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   client.close();
 // });
 async function run() {
-
   try {
     await client.connect();
     const database = client.db("learningManagement");
@@ -30,68 +32,57 @@ async function run() {
     const instructorCollection = database.collection("instructor");
     const popularCoursesCollection = database.collection("popularCourses");
     const reviewsCollection = database.collection("reviews");
- 
 
-   // GET API ALL ORDERS
-   app.get('/all-blogs', async (req,res)=>{
-    const cursor = blogsCollection.find({})
-    const orders = await cursor.toArray();
-    res.send(orders);
-
-   });
-   // GET SINGLE PRODUCT
-   app.get('/all-blog/:id', async (req,res)=>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)};
-    const blogs = await blogsCollection.findOne(query)
-  res.json(blogs);
-  });
-
-   // GET API ALL INSTRUCTOR
-   app.get('/all-instructor', async (req,res)=>{
-    const cursor = instructorCollection.find({})
-    const orders = await cursor.toArray();
-    res.send(orders);
-
-   });
-   // GET API ALL POPULAR COURSES
-   app.get('/all-popCourses', async (req,res)=>{
-    const cursor = popularCoursesCollection.find({})
-    const orders = await cursor.toArray();
-    res.send(orders);
-
-   });
-
+    // GET API ALL ORDERS
+    app.get("/all-blogs", async (req, res) => {
+      const cursor = blogsCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
     // GET SINGLE PRODUCT
-    app.get('/all-popCourse/:id', async (req,res)=>{
+    app.get("/all-blog/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const course = await popularCoursesCollection.findOne(query)
-    res.json(course);
+      const query = { _id: ObjectId(id) };
+      const blogs = await blogsCollection.findOne(query);
+      res.json(blogs);
     });
 
-   // GET API ALL Reviews
-   app.get('/all-Reviews', async (req,res)=>{
-    const cursor = reviewsCollection.find({})
-    const orders = await cursor.toArray();
-    res.send(orders);
+    // GET API ALL INSTRUCTOR
+    app.get("/all-instructor", async (req, res) => {
+      const cursor = instructorCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    // GET API ALL POPULAR COURSES
+    app.get("/all-popCourses", async (req, res) => {
+      const cursor = popularCoursesCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
 
-   });
-        
+    // GET SINGLE PRODUCT
+    app.get("/all-popCourse/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const course = await popularCoursesCollection.findOne(query);
+      res.json(course);
+    });
 
-
-   
-
-   
+    // GET API ALL Reviews
+    app.get("/all-Reviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
   } finally {
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-app.get('/',(req,res)=>{
-  res.send("Running my lms")
-})
+app.get("/", (req, res) => {
+  res.send("Running my lms");
+});
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log("Khul ja sim sim", port);
-})
+});
